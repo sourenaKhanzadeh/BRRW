1bfed3901e652693d23a21f8b74b9d54b9448365
Sourena Khanzadeh <sourena.khanzadeh@gmail.com>
2024-08-24 21:54:23 +0000

    Xperiment101.py added Lama

--- Xperiment101.py
+++ Xperiment101.py
@@ -26,21 +26,29 @@ for folder in folders:
 
         for beam in beams:
             for max_depth in max_depths:
-                search_command = f'--search "ehcbrrw(ipdb(), cost_type=normal, bound=infinity, max_time=infinity, beam_width={beam}, max_depth={max_depth})"'
+                search_command = f'--search "ehcbrrw(add(), cost_type=normal, bound=infinity, max_time=infinity, beam_width={beam}, max_depth={max_depth})"'
                 full_command = f"{command} {search_command}"
                 all_commands.append(full_command)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--build", action="store_true", help="Build the planner before running the experiments.")
 parser.add_argument("--domain", type=int, help="The domain to run the experiments on.")
-
+parser.add_argument("--lama", action="store_true", help="Run the experiments with LAMA instead of BRRW.")
 
 # run command 0
 if parser.parse_args().build:
     print("Building the planner.")
     os.system("./build.py")
     os.system(all_commands[parser.parse_args().domain])
+elif parser.parse_args().lama:
+    print(f"Running the experiments for domain {folders[parser.parse_args().domain]} with LAMA.")
+    command = all_commands[parser.parse_args().domain]
+    command = command.split("--search")
+    command = command[0].replace("./fast-downward.py", "./fast-downward.py --alias seq-sat-lama-2011") 
+    print(command)
+    os.system(command)
 else:
     print(f"Running the experiments for domain {folders[parser.parse_args().domain]}.")
+    print(all_commands[parser.parse_args().domain])
     os.system(all_commands[parser.parse_args().domain])
 
