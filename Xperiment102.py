d35c7e6e773b763856088c8c1ca9a60d100f9ff9
Sourena Khanzadeh <sourena.khanzadeh@gmail.com>
2024-08-31 13:15:04 -0400

    Beam RRW modified (Works Now)
    different way of adopting taking the nodes in
    the beam

--- Xperiment102.py
+++ Xperiment102.py
@@ -96,6 +96,7 @@ parser.add_argument("--all", action="store_true", help="Run the experiments for 
 parser.add_argument("--beam", type=int, help="The beam width to use for the experiments.")
 parser.add_argument("--depth", type=int, help="The max depth to use for the experiments.")
 parser.add_argument("--luby", action="store_true", help="Run the experiments with Luby sequence.")
+parser.add_argument("--l_seq", type=int, help="The Luby sequence value to use for the experiments.")
 
 args = parser.parse_args()
 
@@ -151,9 +152,9 @@ if args.domain is not None and args.problem is not None and args.task is not Non
         if args.ehc and not args.luby:
             command = f"./fast-downward.py misc/tests/benchmarks/{folders[args.domain]}/domain.pddl misc/tests/benchmarks/{folders[args.domain]}/task{two_digit_problem}.pddl --search 'ehc(add(), cost_type=normal, bound=infinity, max_time=10)'"
         elif args.luby:
-            command = f"./fast-downward.py misc/tests/benchmarks/{folders[args.domain]}/domain.pddl misc/tests/benchmarks/{folders[args.domain]}/task{two_digit_problem}.pddl --search 'ehcbrrw(add(), cost_type=normal, bound=infinity, max_time=5, beam_width={args.beam}, max_depth={args.depth}, restart_strategy=\"luby\")'"
+            command = f"./fast-downward.py misc/tests/benchmarks/{folders[args.domain]}/domain.pddl misc/tests/benchmarks/{folders[args.domain]}/task{two_digit_problem}.pddl --search 'ehcbrrw(add(), cost_type=normal, bound=infinity, max_time=infinity, beam_width={args.beam}, max_depth={args.depth}, restart_strategy=\"luby\", luby_start_value={args.l_seq})'"
         else:
-            command = f"./fast-downward.py misc/tests/benchmarks/{folders[args.domain]}/domain.pddl misc/tests/benchmarks/{folders[args.domain]}/task{two_digit_problem}.pddl --search 'ehcbrrw(add(), cost_type=normal, bound=infinity, max_time=5, beam_width={args.beam}, max_depth={args.depth})'"
+            command = f"./fast-downward.py misc/tests/benchmarks/{folders[args.domain]}/domain.pddl misc/tests/benchmarks/{folders[args.domain]}/task{two_digit_problem}.pddl --search 'ehcbrrw(add(), cost_type=normal, bound=infinity, max_time=infinity, beam_width={args.beam}, max_depth={args.depth})'"
         os.system(command)
 
         print(f"Task completed: {args.task}")
